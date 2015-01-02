1d6a7bf5e858fdc53752afc6808937067543df51
JaakkoLipsanen <jaakko.lipsanen@outlook.com>
2015-01-02 20:12:28 +0200

    Added GetSystemTime to Time.h

--- Elbaf.Engine/Source/Engine/Time.cpp
+++ Elbaf.Engine/Source/Engine/Time.cpp
@@ -3,6 +3,8 @@
 #include <Engine\TimeModule.h>
 #include <Core\IEngine.h>
 
+#include <chrono>
+
 static TimeModule& GetTimeModule()
 {
 	return IEngine::GetInstance().GetModule<TimeModule>();
@@ -16,6 +18,24 @@ float Time::GetDeltaTime()
 float Time::GetTotalTime()
 {
 	return GetTimeModule().GetTotalTime();
+}
+
+int Time::GetSystemTime()
+{
+	std::chrono::milliseconds ms = std::chrono::duration_cast<std::chrono::milliseconds >(
+		std::chrono::high_resolution_clock::now().time_since_epoch()
+		);
+
+	return ms.count();
+}
+
+long long Time::GetSystemTimeInNano()
+{
+	std::chrono::nanoseconds ns = std::chrono::duration_cast<std::chrono::nanoseconds >(
+		std::chrono::high_resolution_clock::now().time_since_epoch()
+		);
+
+	return ns.count();
 }
 
 double Time::GetCurrentSystemTime()
